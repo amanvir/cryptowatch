3d27f4b6ce59253ce78e6b5f1f001489c92e2457
amanvir <amanvir.sangha@gmail.com>
2016-12-29 00:31:27 +0000

    Add pricing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import fetch from 'node-fetch'
 export default class Cryptowatch {
 
   constructor () {
-    this.url = 'https://api.cryptowatch.com/'
+    this.url = 'https://api.cryptowatch.com'
   }
 
   allowance () {
     return fetch(this.url)
       .then(r => { return r.json() })
       .then(j => { return j.allowance })
+  }
+
+  price (coin, currency = 'usd', market = 'coinbase') {
+
+    //the cryptowatch api times out
+    //when incorrect params supplied
+    //todo: ask them why
+    const options = {
+      timeout : 5000
+    }
+
+    return fetch(this.url + '/markets/' + market + '/' + coin + currency + '/price', options)
+      .then(r => { return r.json() })
+      .then(j => { return j.result })
+      .catch(err => { throw new TypeError('This endpoint does not exist')  })
   }
 }
--- test/test.js
+++ test/test.js
@@ -19,3 +19,29 @@ test('it gets correct allowance result', t => {
       t.is(a.cost, 111)
     })
 })
+
+test('it gets correct price', t => {
+  nock('https://api.cryptowatch.com')
+    .get('/markets/coinbase/btcusd/price')
+    .reply(200, {'result': {'price': 982.6}, 'allowance': {'cost': 884009, 'remaining': 1947776255}})
+
+  return t.context.cw
+    .price('btc')
+    .then(p => {
+      t.is(p.price, 982.6)
+    })
+})
+
+test('it throws an error when wrong params added', t => {
+
+  nock('https://api.cryptowatch.com')
+    .get('/markets/coinbase-is-not-real/btc-lol-jkcurrency-does-not-exist/price')
+    .delayConnection(5500) // actual timeout in code is 5s
+    .reply(500)
+
+  const error = t.throws(() => {
+    return t.context.cw.price('btc-lol-jk', 'currency-does-not-exist', 'coinbase-is-not-real')
+  }, TypeError)
+
+  t.is(error.message, 'This endpoint does not exist')
+})
