57c5ba9dff0e5447260249bab24304d8544eab9e
Joe Fleming <joe.fleming@gmail.com>
2017-05-20 15:58:30 -0700

    add remaining api endpoints

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import fetch from 'node-fetch'
 
+function fetchResult (url, options) {
+  return fetch(url, options)
+    .then(r => { return r.json() })
+    .then(j => { return j.result })
+    .catch(err => { throw new TypeError('This endpoint does not exist') })
+}
+
 class Cryptowatch {
 
   constructor (options = {}) {
@@ -21,16 +28,37 @@ class Cryptowatch {
 
   // https://cryptowat.ch/docs/api#price
   price (coin, currency = 'usd', market = 'coinbase') {
-    return fetch(`${this.url}/markets/${market}/${coin}${currency}/price`, this.options)
-      .then(r => { return r.json() })
-      .then(j => { return j.result })
-      .catch(err => { throw new TypeError('This endpoint does not exist') })
+    return fetchResult(`${this.url}/markets/${market}/${coin}${currency}/price`, this.options)
   }
 
-    return fetch(`${this.url}/markets/${market}/${coin}${currency}/price`, options)
-      .then(r => { return r.json() })
-      .then(j => { return j.result })
-      .catch(err => { throw new TypeError('This endpoint does not exist') })
+  // https://cryptowat.ch/docs/api#summary
+  summary (coin, currency = 'usd', market = 'coinbase') {
+    return fetchResult(`${this.url}/markets/${market}/${coin}${currency}/summary`, this.options)
+  }
+
+  // https://cryptowat.ch/docs/api#trades
+  trades (coin, currency = 'usd', market = 'coinbase') {
+    return fetchResult(`${this.url}/markets/${market}/${coin}${currency}/trades`, this.options)
+  }
+
+  // https://cryptowat.ch/docs/api#orderbook
+  orderbook (coin, currency = 'usd', market = 'coinbase') {
+    return fetchResult(`${this.url}/markets/${market}/${coin}${currency}/orderbook`, this.options)
+  }
+
+  // https://cryptowat.ch/docs/api#ohlc
+  ohlc (coin, currency = 'usd', market = 'coinbase') {
+    return fetchResult(`${this.url}/markets/${market}/${coin}${currency}/ohlc`, this.options)
+  }
+
+  // https://cryptowat.ch/docs/api#prices
+  prices () {
+    return fetchResult(`${this.url}/markets/prices`, this.options)
+  }
+
+  // https://cryptowat.ch/docs/api#summaries
+  summaries () {
+    return fetchResult(`${this.url}/markets/summaries`, this.options)
   }
 }
 
