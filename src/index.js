6bf6ed4d3a1270281b5386bdfe03c7eae22c5f53
Joe Fleming <joe.fleming@gmail.com>
2017-05-20 15:52:09 -0700

    move options into constructor

--- src/index.js
+++ src/index.js
@@ -2,23 +2,30 @@ import fetch from 'node-fetch'
 
 class Cryptowatch {
 
-  constructor () {
+  constructor (options = {}) {
     this.url = 'https://api.cryptowat.ch'
+    this.options = Object.assign({
+      // the cryptowatch api times out
+      // when incorrect params supplied
+      // todo: ask them why
+      timeout: 5000
+    }, options)
   }
 
+  // https://cryptowat.ch/docs/api#rate-limit
   allowance () {
     return fetch(this.url)
       .then(r => { return r.json() })
       .then(j => { return j.allowance })
   }
 
+  // https://cryptowat.ch/docs/api#price
   price (coin, currency = 'usd', market = 'coinbase') {
-    // the cryptowatch api times out
-    // when incorrect params supplied
-    // todo: ask them why
-    const options = {
-      timeout: 5000
-    }
+    return fetch(`${this.url}/markets/${market}/${coin}${currency}/price`, this.options)
+      .then(r => { return r.json() })
+      .then(j => { return j.result })
+      .catch(err => { throw new TypeError('This endpoint does not exist') })
+  }
 
     return fetch(`${this.url}/markets/${market}/${coin}${currency}/price`, options)
       .then(r => { return r.json() })
